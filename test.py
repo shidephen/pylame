6d74bdc52b02b1a5793c1426d9336c43528e948f
shidephen <shidephen@outlook.com>
2019-01-26 23:19:59 +0800

    Test speed with pydub

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
 from pylame import MP3Encoder
 from pydub import AudioSegment
+import io
 import numpy as np
 from time import time
 
-w = AudioSegment.from_wav("../test.wav")
+w = AudioSegment.from_wav("test.wav")
 encoder = MP3Encoder(w.frame_rate, 320, 2)
 
-buf = np.array(w.get_array_of_samples())
+samples = np.array(w.get_array_of_samples())
 
 start = time()
-o = encoder.encode_interleaved(buf)
-es = time() - start
+for i in range(50):
+    o = encoder.encode_interleaved(samples)
+print(f'pylame costs {time() - start}')
 
-print("Encoding costs: {}s".format(es))
+start = time()
+buf = io.BytesIO()
+for i in range(50):
+    buf.seek(0)
+    w.export(buf, format='mp3')
+print(f'Pydub costs: {time() - start}s')
 
 with open('out.mp3', 'wb') as f:
     f.write(o)
-
-
