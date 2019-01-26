5e660c2e5171b7d63ba8769f8b08e55b262a0f3e
shidephen <shidephen@outlook.com>
2019-01-26 23:47:54 +0800

    version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(name='pylame',
       packages=setuptools.find_packages(),
-      version="0.1",
+      version="0.2",
       package_data={'pylame':['*.so']},
       include_package_data=True,
       )
