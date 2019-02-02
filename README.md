81d0d64b61c8375dc7f894fef95b17cb1e02cafa
shidephen <shidephen@outlook.com>
2019-02-02 18:06:42 +0800

    Add new test

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ Python wrapped of mp3lame encoder
 from pylame import MP3Encoder
 from numpy import *
 
-samples = random.rand(100)
+samples = random.rand(1000)
+samples = ((samples - 0.5) * 32768).astype(int16)
+
 # MP3Encoder(samplerate, bitrate, channels)
-enc = MP3Encoder(44100, 320, 1)
+enc = MP3Encoder(16000, 320, 2)
 out = encoder.encode_interleaved(samples)
 with open('out.mp3', 'wb') as f:
     f.write(o)
--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,13 @@
 from pylame import MP3Encoder
-from pydub import AudioSegment
-import io
-import numpy as np
-from time import time
+from numpy import *
 
-w = AudioSegment.from_wav("test.wav")
-encoder = MP3Encoder(w.frame_rate, 320, 2)
+samples = random.rand(1000)
+samples = ((samples - 0.5) * 32768).astype(int16)
 
-samples = np.array(w.get_array_of_samples())
-
-start = time()
-for i in range(50):
-    o = encoder.encode_interleaved(samples)
-print(f'pylame costs {time() - start}')
-
-start = time()
-buf = io.BytesIO()
-for i in range(50):
-    buf.seek(0)
-    w.export(buf, format='mp3')
-print(f'Pydub costs: {time() - start}s')
-
+# MP3Encoder(samplerate, bitrate, channels)
+enc = MP3Encoder(16000, 320, 2)
+out = enc.encode_interleaved(samples)
+print('Encoded:')
+print(out)
 with open('out.mp3', 'wb') as f:
-    f.write(o)
+    f.write(out)
